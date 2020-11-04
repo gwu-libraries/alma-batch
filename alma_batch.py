010e64f353dbca0fef720f770281dfbe6255d9ef
Dolsy Smith <dsmith@gwu.edu>
2020-11-04 09:55:24 -0500

    Added default logging

--- alma_batch.py
+++ alma_batch.py
@@ -10,25 +10,19 @@ from pathlib import Path
 from alma_batch_utils import *
 from uuid import uuid4
 from datetime import datetime
-from functools import partial
 import traceback
 import sys
 import logging
 
-handler = logging.FileHandler('./alma_batch_log.txt', 'w')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s:%(message)s')
-handler.setFormatter(formatter)
-logger = logging.getLogger('AlmaBatch')
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
 
 class AlmaBatch:
     
     def __init__(self, config: str):
-        '''
-            config: A path to a YAML config file that includes the following
+        '''Initializes the class.
+
+        :param config: A path to a YAML config file that includes the following
                     - the user's API key
-                    - the type of API query (as specified by an EX Libris API endpoint)
+                    - the type of API query (as specified by an EX Libris API endpoint).
         '''
         self._load_config(config)
         self.api_doc = self._load_openapi(self.openapi)
@@ -38,18 +32,25 @@ class AlmaBatch:
         self.batch_idx = 0 # Tracks the number processed in each batch (when batching requests)
         self.num_workers = 25 # Default value for number of async workers
         self.limit = 100 # Number of results per page (default is Ex Libris maximum)
+        self.logger = logging.getLogger(__name__)
     
-    def _load_config(self, path: str):
+    def _load_config(self, config_path: str='', config: dict=None):
         '''Loads the config file. 
-                 path: should point to a YAML file.'''
-        # Required elements in the config file
+        
+        :param config_path: should point to a YAML file
+        :param config: should be a dict of options
+        '''
+        # Required elements in the config file/object
         required_keys = ['apikey', 
                         'endpoint',
                         'operation',
                         'openapi']
         try:
-            with open(path, 'r') as f:
-                config = yaml.load(f, Loader=yaml.FullLoader)
+            if config_path:
+                with open(config_path, 'r') as f:
+                    config = yaml.load(f, Loader=yaml.FullLoader)
+            if not config:
+                raise MissingConfiguration('No configuration specified.')
             # Check for required config elements
             if not all(k in config for k in required_keys):
                 raise MissingConfiguration('One or more required configuration elements is missing.')
@@ -57,12 +58,14 @@ class AlmaBatch:
             for key, value in config.items():
                 setattr(self, key, value)
         except Exception as e:
-            print('Error loading configuration.')
+            self.logger.exception('Error loading configuration.')
             raise
     
     def _load_openapi(self, openapi: str):
         '''Loads the OpenAPI documentation for the desired endpoint.
-            openapi: either a URL or a local file path. The file should be in JSON format.'''
+        
+        :param openapi: either a URL or a local file path. The file should be in JSON format.
+        '''
         try:
             # tests whether the argument is a URL or not
             if urlparse(openapi).scheme:
@@ -73,12 +76,12 @@ class AlmaBatch:
                 with open(openapi, 'r') as f:
                     return json.load(f)
         except Exception as e:
-            print('Failed to load OpenAPI documentation.')
+            self.logger.exception('Failed to load OpenAPI documentation.')
             raise
     
     def _create_header(self):
-        '''Creates the header for the API request, using the supplied API key from the config file and 
-        (optionally) a supplied accept parameter (default is JSON) and content type (required if operation is put/post)'''
+        '''Creates the header for the API request, using the supplied API key from the config file and (optionally) a supplied accept parameter (default is JSON) and content type (required if operation is put/post).
+        '''
         try:
             header = {'Authorization': f'apikey {self.apikey}'}
             if hasattr(self, 'accepts'):
@@ -90,23 +93,28 @@ class AlmaBatch:
                 header['Content-Type'] = self.content_type
             return header
         except AttributeError:
-            print('Configuration error. "content_type" is a required parameter for POST or PUT operations.')
+            self.logger.error('Configuration error. "content_type" is a required parameter for POST or PUT operations.')
             raise
     
     def load_csv(self, path_to_csv: str, clean_columns: bool = True):
         '''Loads the CSV file at the supplied path and assigns to self.
-        Second argument is a flag for whether the columns should be converted to snake case.'''
+        
+        :param path_to_csv: path to the CSV file (local or via http).
+        :param clean_columns: a flag for whether the columns should be converted to snake case.
+
+        '''
         try:
             self.data = pd.read_csv(path_to_csv)
             if clean_columns:
                 self.data.columns = convert_column_case(self.data.columns)
             return self
         except Exception as e:
-            print('Failed to load CSV.')
+            self.logger.exception('Failed to load CSV.')
             raise e
     
     def validate_data(self):
         '''Checks that the columns or keys in self.data correspond to the parameters in the specified API.'''
+
         # Check to see if the data object has "columns," i.e., is a DataFrame
         if hasattr(self.data, 'columns'):
             columns = set(self.data.columns)
@@ -128,10 +136,14 @@ class AlmaBatch:
             self.query_params = query_params
             return self
         except Exception as e:
-            print('Error validating parameters')
+            self.logger.exception('Error validating parameters')
             raise
 
     def _make_batch(self):
+        '''
+        Batches self.data, using self.batch_size. Each batch of requests will be run concurrently.
+        '''
+
         # If self.data is a DataFrame, convert to native Python structure (list of dicts) for processing
         # Include the index of the row as a separate value
         # For each row, we initialize the page to 0 (for paginated results)
@@ -151,8 +163,11 @@ class AlmaBatch:
         return batches
 
     def _construct_request(self, row_data: dict, page: int):
-        '''Given a dict of keys and values, returns a url and a set of query parameters for an API call.
-        page should be a value for paginated results, or 0 for the first page of results.'''
+        '''Returns a url and a set of query parameters for an API call.
+
+        :param row_data: should contain URL and/or query string parameters for the request, or a complete URL assigned to the key "link." 
+        :param page: should be a value for paginated results, or 0 for the first page of results.
+        '''
 
         # In some cases, we may be able to use the pre-constructed URL returned by another ExL API.
         if 'link' in row_data:
@@ -176,16 +191,20 @@ class AlmaBatch:
         return url, args
 
     def _check_for_pagination(self, result: dict):
-        '''Checks a result from the API for paginated results. If the total_record_count attribute > self.limit, then returns a number of pages still needed. Otherwise, returns 0.'''
+        '''Checks a result from the API for paginated results. If the total_record_count attribute > self.limit, then returns a number of pages still needed. Otherwise, returns 0.
+
+        :param result: result of an API call.
+        '''
         total_results = result.get('total_record_count')
         if total_results and total_results > self.limit:
             return int(total_results / self.limit) # Number of (additional) iterations needed to get the rest of the results
         return 0
 
     def _update_data(self):
-        '''Updates the data supplied as parameters to the API calls, indicating which rows have been completed.'''
+        '''Updates the data supplied as parameters to the API calls, indicating which rows have been completed.
+        '''
         if len(self.results) == 0:
-            print('No successful results to update.')
+            self.logger.debug('No successful results to update.')
             return
         try:
             # If self.data is not already a a DataFrame, convert to one
@@ -220,7 +239,10 @@ class AlmaBatch:
     
     def dump_output(self, batch: int = None):
         '''Saves the API output to disk, as JSON map from row index to result object.
-        If a batch parameter is supplied and the batch_size is non-zero, save the output in batches.'''
+        If a batch parameter is supplied and the batch_size is non-zero, save the output in batches.
+
+        :param batch: number of the batch most recently completed
+        '''
         if self.batch_size > 0:
             # Save everything added since the last batch, if anything was added
             if not len(self.results) > self.batch_idx: 
@@ -243,22 +265,32 @@ class AlmaBatch:
             with open(Path(self.path_for_api_output) / f'api_results_batch-{batch}-{file_id}.json', 'w') as f:
                 json.dump(api_output, f)
         except Exception as e:
-            print("Error saving API output.")
+            self.logger.exception("Error saving API output.")
             raise
         return self
 
     def _do_after_requests(self, iteration: int):
+        '''
+        Called after a completed batch of requests.
+
+        :param iteration: the number of the just-completed batch.
+        '''
         if hasattr(self, 'output_file'):
             self._update_data()
         if hasattr(self, 'path_for_api_output') and self.serialize_return:
-            print(f'Saving API output for batch {iteration+1}')
+            self.logger.info(f'Saving API output for batch {iteration+1}')
             self.dump_output(batch=iteration+1)
     
     async def _async_request(self, row_data: dict, idx: int, page: int=0, payload=None):
-        '''row should be a dict mapping path & query parameters. 
-        idx should be a numeric row index from the original dataset.
-        page should be an integer for paginated results. 
-        If payload is present, it will typically be dictionary, too.'''
+        '''
+        Make a single asynchronous request.
+
+        :param row: a mapping to path & query parameters
+        :param idx: a row index from the original dataset
+        :param page: current page of results (0 if unpaginated)
+        :param payload: for PUT/POST operations
+
+        '''
         url, args = self._construct_request(row_data, page)
         # Optional payload (for PUT/POST)
         # TO DO: Accept XML payloads
@@ -269,7 +301,7 @@ class AlmaBatch:
                 'page': page}
         # Get the client method corresponding to the desired HTTP method
         client_method = getattr(self.client, self.operation)
-        logger.debug(f'Making request for row {idx}, page {page}.')
+        self.logger.debug(f'Making request for row {idx}, page {page}.')
         try:
             # Wrap the call in the throttler context manager
             async with self.throttler:    
@@ -280,7 +312,7 @@ class AlmaBatch:
                         raise APIException(error)
                     elif self.content_type == 'application/json':
                         result = await session.json()
-                        logger.debug(f'Received data for row {idx}, page {page}.')
+                        self.logger.debug(f'Received data for row {idx}, page {page}.')
                     else:
                         result = await session.text()
                         # Save the result
@@ -294,7 +326,7 @@ class AlmaBatch:
             # If this row throws an exception, record it in the errors list
             exc_info = sys.exc_info()
             output['error'] = traceback.format_exception(*exc_info)
-            logger.error(f'Error {e} on row {idx} page {page}.')
+            self.logger.exception(f'Error {e} on row {idx} page {page}.')
             self.errors.append(output)
             # Can't extract pagination in this case
             return 0
@@ -306,7 +338,7 @@ class AlmaBatch:
         '''
         while True:
             request_task = await self.queue.get() # Get a task to process. Request should be a dictionary containing a row of data, an index (idx), and an page 
-            logger.debug(f"Task acquired: index {request_task['idx']}, page {request_task['page']}.")
+            self.logger.debug(f"Task acquired: index {request_task['idx']}, page {request_task['page']}.")
             try:
                 more_pages = await self._async_request(**request_task) 
                 # Mark the current request task as done
@@ -316,16 +348,18 @@ class AlmaBatch:
                     new_task = request_task.copy()
                     new_task['page'] = page + 1
                     self.queue.put_nowait(new_task)
-                    logger.debug(f"Task queued up: index {request_task['idx']}, page {request_task['page']}.")
+                    self.logger.debug(f"Task queued up: index {request_task['idx']}, page {request_task['page']}.")
                 self.queue.task_done()
             except Exception as e:
-                print(f"Aborting request {request_task['idx']} without completion.")
+                self.exception(f"Aborting request {request_task['idx']} without completion.")
                 #self.queue.task_done()
                 raise e
  
     async def _main(self, batch): 
         '''
         Handles adding tasks to the queue, one per request, and then running the queue with the event loop. This function is blocking: each batch will kick off a "new" asynchronous queue of tasks, which will be finished before the next batch proceeds. 
+
+        :param batch: a batch of requests to process
         '''
         self.queue = asyncio.Queue() # Async task queue => keeps track of the number of tasks left to perform
         # Initialize a new client session
@@ -354,12 +388,12 @@ class AlmaBatch:
 
     def make_requests(self, rate_limit: int = 25, batch_size: int = 1000, serialize_return: bool = False):
         '''AManages asynchronous requests in batches.
-        Rate limit is set by default to the max allowed by the ExL API (25 reqs/sec).
-        The batch size can be set to 0 to operate without batching. 
-        Otherwise, asynchronous requests will be batched, and in betweeen batches, if an output file path is specified 
-        in the config file, then the data will be updated and saved to disk as a CSV file. 
-        This allows the user to keep track of which rows have been processed in the event of application crash, serious network interruption, etc.
-        Set the serialize_return flag to true to save the current API ouput to disk, too.'''
+        
+        :param rate_limit: set by default to the max allowed by the ExL API (25 reqs/sec).
+        :param batch_size: can be set to 0 to operate without batching. Otherwise, asynchronous 
+                           requests will be batched, and in betweeen batches, if an output file path is specified in the config file, then the data will be updated and saved to disk as a CSV file. This allows the user to keep track of which rows have been processed in the event of application crash, serious network interruption, etc.
+        :param serialize_return: set to True to save the current API ouput to disk between batches
+        '''
         self.batch_size = batch_size
         self.serialize_return = serialize_return
         # Context manager for throttling the requests
@@ -368,12 +402,12 @@ class AlmaBatch:
         # Run in batches, pausing between each to update the data
         for j, batch in enumerate(batches):
             try:
-                print(f'Running batch {j+1}...')
+                self.logger.info(f'Running batch {j+1}...')
                 asyncio.run(self._main(batch))
                 self._do_after_requests(iteration=j)
             except Exception as e:
-                print(f'Exception encountered on batch {j+1}. Proceeding to next batch.')
+                self.exception(f'Exception encountered on batch {j+1}. Proceeding to next batch.')
                 #continue
                 raise # For debugging
-        print('All requests completed.')
+        self.logger.info('All requests completed.')
         return self
